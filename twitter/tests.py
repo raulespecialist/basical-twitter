400e17ee8a52d4b26ec4714c299b00e0552b140c
raulespecialist <meloadik@gmail.com>
2021-10-14 12:52:32 -0500

    add test for models

--- twitter/tests.py
+++ twitter/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
+from twitter.models import Tweet
+
+class AuthorModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        #Set up non-modified objects used by all test methods
+        Tweet.objects.create(name='Big Mama', tweet='The tweet for unity test')
